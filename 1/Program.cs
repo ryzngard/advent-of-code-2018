468177dd104b782422f709cd785a1185bbbd3733
Andrew Hall (METAL) <andrha@microsoft.com>
2018-12-03 14:54:43 -0800

    Add first solution

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = args.Length > 0 ? args[0] : "input.txt";
+            int frequency = 0;
+
+            foreach (var line in File.ReadAllLines(file))
+            {
+                frequency += parseInt(line);
+            }
+
+            Console.WriteLine($"Final frequency is : {frequency}");
+        }
+
+        static int parseInt(string line)
+        {
+            return int.Parse(line);
+        }
+    }
+}
