c5d187c76d675ed6222e86d0670ab91b7008f42b
Andrew Hall (METAL) <andrha@microsoft.com>
2018-12-03 15:09:52 -0800

    Add solution 2

---
+++ 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = args.Length > 0 ? args[0] : "input.txt";
+            int frequency = 0;
+
+            var lines = File.ReadAllLines(file);
+            HashSet<int> frequencies = new HashSet<int>();
+
+            bool waiting = true;
+            while(waiting)
+            {
+                foreach (var line in lines)
+                {
+                    frequency += parseInt(line);
+
+                    if (frequencies.Contains(frequency))
+                    {
+                        waiting = false;
+                        break;
+                    }
+
+                    frequencies.Add(frequency);
+                }
+            }
+
+            Console.WriteLine($"First frequency encountered twice : {frequency}");
+        }
+
+        static int parseInt(string line)
+        {
+            return int.Parse(line);
+        }
+
+
+    }
+}
